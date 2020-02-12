fcdcf1d627c839d7b00b27b69022a92f7096b65c
paraschakis <paraschakis@gmail.com>
2020-02-12 21:02:07 +0100

    use index.html as starting doc file

--- docs/conf.py
+++ docs/conf.py
@@ -50,12 +50,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
-# html_theme = 'guzzle_sphinx_theme'
-html_theme_options = {
-    "bgcolor": "white"
-}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+master_doc = 'index'
